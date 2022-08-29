2b62a9a68d8cce092f39cdcd449848dde2870192
Michael <michael.jlotter@gmail.com>
2022-08-28 20:43:44 -0400

    Assignment 5 submission
    Submitting Assignment 5

---
+++ Project5_comparisons_type_coercion/main.js
@@ -0,0 +1,55 @@
+//document.write(typeof 3);
+
+//function my_function() {
+  //  document.getElementById("test").innerHTML= isNaN(10/0); //remove 'isnan' and brackets the dispply result will be Nan. this results in 'true' and use any other combo of numbers will result in false.
+//};
+
+//document.write(-3E310) 
+
+//document.write(100000>10000000000000); 
+
+//document.write(2<27)
+
+//console.log(9*9)
+
+//document.write(5 + '10');// doesnt matter if you use the quotations first or the number the reuslt will be a combining of the answer
+
+//console.log(9<=9); //runs in console as true
+
+//document.write (12 == (6+6));
+
+//x = 'taylorMade'
+//y = 'taylorMade'
+//document.write(x === y);
+
+//x = '10'
+//y = 'ten'
+//document.write(x === y);
+
+///x = 'tenpens'
+//y = 'tenpins'
+//document.write (x === y);
+
+//document.write (5<10 && 10<20);
+//document.write (5<10 && 7>100);
+//document.write (5<10 || 10 > 3);
+//document.write (5>10 || 3>10);
+
+function is_not() {
+    document.getElementById('isnt').innerHTML = !(75>133); // this will return ture because it is a duble negative
+}
+
+document.write(typeof 'bananas');
+
+console.log (9 == 0); //shows as flase n concole log
+
+x = (12 + 9)
+y = (9 + 12)
+document.write (x === y); //will show as true after the computation is completed
+
+x = 98
+y = 99
+document.write (x < y || x > Y); //shows true
+document.write (x <=y && y > x); // shows true
+
+
