e0ca84a1b58769e5ece9c3f51e71cd188561cd9f
Michael <michael.jlotter@gmail.com>
2022-09-01 13:47:39 -0400

    Submit assignment 9
    Submit assignment 9

---
+++ Project9_countdown_slideshow/main.js
@@ -0,0 +1,28 @@
+/*function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds -1;
+        timer.innerHTML = seconds;
+        setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("time is up");
+        }
+    }
+    tick();
+}*/
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides= document.getElementsByClassName("mySlides");
+    for (i= 0; i <slides.length; i++){
+        slides [i].style.display= "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex =1}
+    slides[slideIndex -1].style.display ="block";
+    setTimeout(showSlides, 200); //slides rotate from this instruction, can make it faster with smaller number because measured in milliseconds
+}
